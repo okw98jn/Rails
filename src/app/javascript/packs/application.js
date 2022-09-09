7ac35cd18342ff3f11d5502c902533cf384bd43f
jin <0cu2045d@gmail.com>
2022-09-09 22:45:08 +0900

    bootstrap,jquery導入

--- src/app/javascript/packs/application.js
+++ src/app/javascript/packs/application.js
@@ -8,6 +8,10 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import "jquery";
+import "bootstrap";
+import "../src/application";
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
