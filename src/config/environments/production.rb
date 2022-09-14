b06935cffb1a4290e238386466e6c8f287ccd1f3
jin <0cu2045d@gmail.com>
2022-09-14 19:05:51 +0900

    本番環境でメール送信の際エラーが出るため

--- src/config/environments/production.rb
+++ src/config/environments/production.rb
@@ -62,6 +62,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "app_production"
   config.action_mailer.default_url_options = { host: 'example.com'}
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_caching = true
 
   config.action_mailer.smtp_settings = {
