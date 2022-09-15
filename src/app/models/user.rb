06b8d353c2c2f844fd11c81253e6d951515b1b5e
jin <0cu2045d@gmail.com>
2022-09-15 23:28:39 +0900

    バリデーションを独自で設定

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable
 
   def self.guest
     find_or_create_by!(email: 'aaa@aaa.com') do |user|
@@ -11,4 +11,8 @@ class User < ApplicationRecord
       user.name = 'ゲストさん'
     end
   end
+
+  validates :name, presence: true, length: { maximum: 10, allow_blank: true }
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, allow_blank: true }
+  validates :password, presence: true, length: { minimum: 6, allow_blank: true }, on: :create, confirmation: true
 end
