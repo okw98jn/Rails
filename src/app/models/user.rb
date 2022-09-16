d12f8cf2a087423d3427f42e6b2bf36463468ab4
jin <0cu2045d@gmail.com>
2022-09-16 23:16:03 +0900

    パスワード更新の際にバリデーションをかけるため

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -14,5 +14,5 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 10, allow_blank: true }
   validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, allow_blank: true }
-  validates :password, presence: true, length: { minimum: 6, allow_blank: true }, on: :create, confirmation: true
+  validates :password, presence: true, length: { minimum: 6, allow_blank: true }, confirmation: true
 end
