91a38c4cf4a4a24a9ed38d074af63e4df14e1670
jin <0cu2045d@gmail.com>
2022-09-20 00:47:12 +0900

    パスワード無しで編集できる用に設定

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -12,7 +12,20 @@ class User < ApplicationRecord
     end
   end
 
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
+
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update(params, *options)
+    clean_up_passwords
+    result
+  end
+
   validates :name, presence: true, length: { maximum: 10, allow_blank: true }
   validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, allow_blank: true }
-  validates :password, presence: true, length: { minimum: 6, allow_blank: true }, confirmation: true
+  validates :password, presence: true, length: { minimum: 6, allow_blank: true }, confirmation: true, on: :create
 end
