ba4100bec992af4f61eb19b4eb160634339f217d
jin <0cu2045d@gmail.com>
2022-09-19 18:36:12 +0900

    ユーザー編集ページの設定

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit]
 end
