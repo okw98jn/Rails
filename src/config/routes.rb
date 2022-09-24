f68ffa260d85dc3746aec416e4cdb4e487382eab
jin <0cu2045d@gmail.com>
2022-09-24 16:30:48 +0900

    ユーザー一覧画面作成

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
-  resources :users, only: [:show]
+  resources :users, only: [:show, :index]
   # 退会ページ
   get '/users/:id/unsubscribe', to: 'users#unsubscribe', as: 'unsubscribe'
   # 退会処理
