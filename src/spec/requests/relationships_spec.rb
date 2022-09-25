8a510b01ac7aae199859cb13c0d413946572ccd6
jin <0cu2045d@gmail.com>
2022-09-25 22:26:32 +0900

    テスト未実装のためpendingに変更

--- src/spec/requests/relationships_spec.rb
+++ src/spec/requests/relationships_spec.rb
@@ -2,16 +2,10 @@ require 'rails_helper'
 
 RSpec.describe "Relationships", type: :request do
   describe "GET /followings" do
-    it "returns http success" do
-      get "/relationships/followings"
-      expect(response).to have_http_status(:success)
-    end
+    pending "add some examples (or delete) #{__FILE__}"
   end
 
   describe "GET /followers" do
-    it "returns http success" do
-      get "/relationships/followers"
-      expect(response).to have_http_status(:success)
-    end
+    pending "add some examples (or delete) #{__FILE__}"
   end
 end
