5b4889396db7163f4c3c45264d5d9defddee704f
jin <0cu2045d@gmail.com>
2022-09-25 22:07:12 +0900

    フォロー機能のルーティング設定

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -7,7 +7,13 @@ Rails.application.routes.draw do
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
-  resources :users, only: [:show, :index]
+
+  resources :users, only: [:show, :index] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings', to: 'relationships#followings', as: 'followings'
+    get 'followers', to: 'relationships#followers', as: 'followers'
+  end
+
   # 退会ページ
   get '/users/:id/unsubscribe', to: 'users#unsubscribe', as: 'unsubscribe'
   # 退会処理
