753f350aae5eda178fc6b87cd7bfdb8a5af01074
jin <0cu2045d@gmail.com>
2022-10-10 02:07:36 +0900

    cocoon導入

--- src/app/javascript/packs/application.js
+++ src/app/javascript/packs/application.js
@@ -13,6 +13,9 @@ import "bootstrap";
 import "@fortawesome/fontawesome-free/js/all";
 import "../src/application";
 
+require("jquery");
+require("@nathanvda/cocoon")
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
