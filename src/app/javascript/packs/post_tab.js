c437f86a06f581a08abd57328cccd903fd2de985
jin <0cu2045d@gmail.com>
2022-10-13 21:35:41 +0900

    リロードしないとクリックイベントが発火しなかったため

--- src/app/javascript/packs/post_tab.js
+++ src/app/javascript/packs/post_tab.js
@@ -1,11 +1,13 @@
-$(function () {
-  $(".tab").on("click", function () {
-    $(".tab").removeClass("post_tab_active");
-    $(".post_content").removeClass("content_active");
+$(document).on("turbolinks:load", function () {
+  $(function () {
+    $(".tab").on("click", function () {
+      $(".tab").removeClass("post_tab_active");
+      $(".post_content").removeClass("content_active");
 
-    $(this).addClass("post_tab_active");
+      $(this).addClass("post_tab_active");
 
-    const index = $(".tab").index(this);
-    $(".post_content").eq(index).addClass("content_active");
+      const index = $(".tab").index(this);
+      $(".post_content").eq(index).addClass("content_active");
+    });
   });
 });
