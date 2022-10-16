6c1759c402c34157a645ad57e5c0c3eb73f759e7
jin <0cu2045d@gmail.com>
2022-10-16 20:21:59 +0900

    レシピ投稿機能実装

--- src/app/controllers/posts_controller.rb
+++ src/app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :cannot_changed_other_user_post, only: [:show, :edit, :update, :destroy]
 
   def new
     @post = Post.new
@@ -21,11 +23,46 @@ class PostsController < ApplicationController
   def index
   end
 
+  def show
+    @materials = @post.materials.where.not(material_name: "")
+    @procedures = @post.procedures.where.not(process_image: "", explanation: "")
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      flash[:notice] = "投稿を編集しました"
+      redirect_to post_path(@post.id)
+    else
+      flash[:danger] = @post.errors.full_messages
+      redirect_to edit_post_path(@post.id)
+    end
+  end
+
+  def destroy
+    @post.destroy
+    flash[:notice] = "投稿を削除しました"
+    redirect_to user_path(current_user.id)
+  end
+
   protected
 
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
   def post_params
-    params.require(:post).permit(:title, :post_image, :description,
+    params.require(:post).permit(:title, :post_image, :description, :time, :number_of_persons,
                                 materials_attributes: [:id, :material_name, :quantity, :_destroy],
-                                procedures_attributes: [:id, :explanation, :process_image]).merge(user_id: current_user.id)
+                                procedures_attributes: [:id, :explanation, :process_image, :_destroy]).merge(user_id: current_user.id)
+  end
+
+  # 他のユーザーの投稿は編集できない
+  def cannot_changed_other_user_post
+    unless @post.user_id == current_user.id
+      redirect_to user_path(current_user.id), alert: '他のユーザーの投稿は編集できません'
+    end
   end
 end
