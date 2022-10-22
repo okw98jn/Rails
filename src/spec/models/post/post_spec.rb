79e97ec6b2d2acaa443c4bf86318f4de303359c4
jin <0cu2045d@gmail.com>
2022-10-22 17:47:31 +0900

    投稿機能のテスト記述

--- src/spec/models/post/post_spec.rb
+++ src/spec/models/post/post_spec.rb
@@ -1,5 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "バリデーション" do
+    let(:user) { FactoryBot.build(:user) }
+    let(:post) { FactoryBot.build(:post, user: user) }
+
+    describe "title" do
+      context "titleが入力されている場合" do
+        it "有効となること" do
+          post.title = "hoge"
+          expect(post).to be_valid
+        end
+      end
+
+      context "titleが入力されていない場合" do
+        it "無効となること" do
+          post.title = ""
+          expect(post).to be_invalid
+          expect(post.errors).to be_of_kind(:title, :blank)
+        end
+      end
+
+      context "30文字以内の場合" do
+        it "有効となること" do
+          post.title = "a" * 30
+          expect(post).to be_valid
+        end
+      end
+
+      context "30文字以上の場合" do
+        it "無効となること" do
+          post.title = "a" * 31
+          expect(post).to be_invalid
+          expect(post.errors).to be_of_kind(:title, :too_long)
+        end
+      end
+    end
+
+    describe "post_image" do
+      context "post_imageが入力されている場合" do
+        it "有効となること" do
+          post.post_image = Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test.jpg'))
+          expect(post).to be_valid
+        end
+      end
+
+      context "post_imageが入力されていない場合" do
+        it "無効となること" do
+          post.post_image = ""
+          expect(post).to be_invalid
+          expect(post.errors).to be_of_kind(:post_image, :blank)
+        end
+      end
+    end
+  end
 end
--- src/spec/requests/post/posts_spec.rb
+++ src/spec/requests/post/posts_spec.rb
@@ -1,7 +1,69 @@
 require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:user) { FactoryBot.create(:user) }
+  let(:post) { FactoryBot.create(:post) }
+
+  describe "posts#new" do
+    context "ログイン済みユーザーの場合" do
+      it "正常にレスポンスを返すこと" do
+        sign_in user
+        get new_post_path
+        expect(response).to be_successful
+      end
+    end
+
+    context "ログインしていないユーザーの場合" do
+      it "ログインページにリダイレクトされること" do
+        get new_post_path
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+  end
+
+  describe "posts#show" do
+    context "ログイン済みユーザーの場合" do
+      it "正常にレスポンスを返すこと" do
+        sign_in user
+        get post_path(post.id)
+        expect(response).to be_successful
+      end
+    end
+
+    context "ログインしていないユーザーの場合" do
+      it "ログインページにリダイレクトされること" do
+        get post_path(post.id)
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+  end
+
+  describe "posts#edit" do
+    let(:another_user) { FactoryBot.create(:user) }
+    let(:post) { FactoryBot.create(:post, user: user) }
+    let(:another_post) { FactoryBot.create(:post, user: another_user) }
+
+    context "自分の投稿の場合" do
+      it "正常にレスポンスを返すこと" do
+        sign_in user
+        get edit_post_path(post.id)
+        expect(response).to be_successful
+      end
+    end
+
+    context "他人の投稿の場合" do
+      it "マイページにリダイレクトされること" do
+        sign_in user
+        get edit_post_path(another_post.id)
+        expect(response).to redirect_to user_path(user.id)
+      end
+    end
+
+    context "ログインしていないユーザーの場合" do
+      it "ログインページにリダイレクトされること" do
+        get edit_post_path(post.id)
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
   end
 end
