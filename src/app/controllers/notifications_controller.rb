38fdedeccc02a47b809751b756f874f184776065
jin <0cu2045d@gmail.com>
2022-11-03 23:33:42 +0900

    ページネーション追加

--- src/app/controllers/notifications_controller.rb
+++ src/app/controllers/notifications_controller.rb
@@ -2,7 +2,7 @@ class NotificationsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @notifications = current_user.passive_notifications.order(created_at: :desc).where.not(visitor_id: current_user.id)
+    @notifications = current_user.passive_notifications.order(created_at: :desc).where.not(visitor_id: current_user.id).page(params[:page]).per(10)
     @notifications.where(checked: false).each do |notification|
       notification.update(checked: true)
     end
