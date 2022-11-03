800cf9a3ba9f61ced887e6acd9427d43f9281535
jin <0cu2045d@gmail.com>
2022-11-03 23:33:16 +0900

    通知アイコン設定

--- src/app/helpers/notifications_helper.rb
+++ src/app/helpers/notifications_helper.rb
@@ -2,21 +2,21 @@ module NotificationsHelper
   def notification_form(notification)
     visitor = notification.visitor
     visited = notification.visited
-    @comment = nil
-    visitor_comment = notification.comment_id
     case notification.action
     when 'follow'
       tag.a(notification.visitor.name, href: user_path(visitor)) + 'さんがあなたをフォローしました'
     when 'like'
       tag.a(notification.visitor.name, href: user_path(visitor)) + 'さんが' + tag.a('あなたの投稿', href: post_path(notification.post_id)) + 'にいいねしました'
     when 'comment' then
-      @comment = Comment.find_by(id: visitor_comment)
-      @comment_content = @comment.body
       if notification.post.user_id == visited.id
         tag.a(visitor.name, href: user_path(visitor)) + 'さんが' + tag.a("あなたの投稿", href: post_path(notification.post_id)) + 'にコメントしました'
       else
         tag.a(visitor.name, href: user_path(visitor)) + 'さんが' + tag.a(image_tag(notification.post.user.user_image.url) + notification.post.user.name + 'さんのレシピ', href: post_path(notification.post_id), class: 'second_user') + 'にコメントしました'
       end
     end
+  end
+
+  def unchecked_notifications
+    notifications = current_user.passive_notifications.where(checked: false)
   end
 end
