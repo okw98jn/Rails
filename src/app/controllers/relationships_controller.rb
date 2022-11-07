3404fb9729905ead0b51350edf18cc6c91c56744
jin <0cu2045d@gmail.com>
2022-11-07 23:26:51 +0900

    ページネーションの表示人数を８件に変更

--- src/app/controllers/relationships_controller.rb
+++ src/app/controllers/relationships_controller.rb
@@ -18,12 +18,12 @@ class RelationshipsController < ApplicationController
   # フォロー一覧
   def followings
     user = User.find(params[:user_id])
-    @users = user.followings.page(params[:page]).per(4)
+    @users = user.followings.page(params[:page]).per(8)
   end
 
   # フォロワー一覧
   def followers
     user = User.find(params[:user_id])
-    @users = user.followers.page(params[:page]).per(4)
+    @users = user.followers.page(params[:page]).per(8)
   end
 end
